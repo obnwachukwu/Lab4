7cbb7b159a40bdad0fea2644fd7b0f189bee5b3a
unknown <g00435305@atu.ie>
2025-02-26 11:39:42 +0000

    Lab4

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; //importing the CommonModule
+@Component({
+  selector: 'app-list',
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+//list of the presidents and terms
+presidents: any[] =
+[{name: "Higgins", term:"2011-"},
+{name: "McAleese", term:"1997-2011"},
+{name: "Robinson", term:"1990-1997"},
+{name: "Hillery", term:"1976-1990"},
+{name: "O Dalaigh", term:"1974-1976"}];
+
+
+}
+
